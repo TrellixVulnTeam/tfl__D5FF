2201b9da43e68b779a43d9af2442bfebb4486360
Filip <mazza.barca@gmail.com>
2019-02-04 08:06:11 +0100

    c

--- src/product_quantities/admin.py
+++ src/product_quantities/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import ProductQuantity
 
-admin.site.register(ProductQuantity)
+
+class ProductQuantityAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'product', 'quantity']
+
+    class Meta:
+        model = ProductQuantity
+
+
+admin.site.register(ProductQuantity, ProductQuantityAdmin)
--- src/product_quantities/models.py
+++ src/product_quantities/models.py
@@ -1,18 +1,30 @@
 from django.db import models
+from django.db.models import Q
 
 from carts.models import Cart
 from products.models import Product
 
 
+class ProductQuantityQuerySet(models.query.QuerySet):
+    def exist(self, cart_obj, product_obj):
+        lookups = (Q(cart=cart_obj) &
+                   Q(product=product_obj))
+        return self.filter(lookups).first()
+
+
 class ProductQuantityManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuantityQuerySet(self.model, using=self._db)
+
     def new(self, cart_obj, product_obj):
-        self.model.objects.create(cart=cart_obj, product=product_obj)
+        if (self.get_queryset().exist(cart_obj, product_obj)) is None:
+            self.model.objects.create(cart=cart_obj, product=product_obj)
 
 
 class ProductQuantity(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=1)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
