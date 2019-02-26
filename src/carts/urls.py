2f9d9f49c7fdfb7fdffcc0a7cf1a63310dd8a414
Filip <mazza.barca@gmail.com>
2019-02-26 08:10:57 +0100

    c

--- src/carts/urls.py
+++ src/carts/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from .views import (
         CartView,
         CartRemoveView,
-        # cart_home,
         cart_add,
-        # cart_remove,
         checkout_home
 )
 
